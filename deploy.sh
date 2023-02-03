867bb426b224bb2d72bc0215ef35a6ee6a28b15a
Tao0929 <878499007@qq.com>
2023-02-03 11:52:22 +0800

    feat: ui库文档

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+# 忽略错误
+set -e
+# 安装依赖
+npm install
+# 构建
+npm run docs:build
+
+# 进入待发布的目录
+cd .vitepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+git config --global user.email 878499007@qq.com
+git config --global user.name Tao0929
+git init
+git add -A
+git commit -m 'deploy docs'
+
+# 如果部署到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果是部署到 https://<USERNAME>.github.io/<REPO>
+# git config receive.denyCurrentBranch ignore
+
+git push -f git@github.com:Tao0929/easy-draw-docs.git master:gh-pages
+
+cd -
---
+++ guide/index.md
@@ -0,0 +1,8 @@
+### 组件库介绍
+- 作者： 吴涛 Tao
+
+基于 Vue 3，让前端人员简简单单画UI！！！
+
+    hhh 稍后补全。。。凑合看
+
+![An image](../assets/sitelogo.png)
